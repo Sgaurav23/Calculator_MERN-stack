45f54d17e6042c335c9376cafd4832720022d15d
Gaurav <sonargaurav23@gmail.com>
2024-10-10 20:08:15 +0530

    Your commit message

--- calculator/frontend/vite.config.js
+++ calculator/frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/Calculator_MERN-stack/',
+  plugins: [react()],
   server:{
     proxy: {
       '/api': 'http://localhost:3000',
     },
-  },
-  plugins: [react()],
+  }
 })
