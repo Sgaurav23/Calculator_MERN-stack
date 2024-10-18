64ecb6a2b5476ffafcb3d2f33390f2f2b0af6792
Gaurav <sonargaurav23@gmail.com>
2024-10-18 13:06:32 +0530

    Deploy updated calculator to GitHub Pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Calculator from './calculator'
+function App() {
+    return (
+        <div>
+            <Calculator />
+        </div>
+    )
+}
+
+export default App;
