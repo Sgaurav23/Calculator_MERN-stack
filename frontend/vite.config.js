ea6e913c4e077b326413b0a1dd554e7636d87e67
Gaurav <sonargaurav23@gmail.com>
2024-10-18 14:39:53 +0530

    Redeploy updated calculator to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/Calculator_MERN-stack/',
+//   plugins: [react()],
+//   server:{
+//     proxy: {
+//       '/api': 'http://localhost:3000',
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       external: ['react-router-dom']
+//     }
+//   }
+// })
+
+
+
+
+
+
+// export default {
+//   base: '/Calculator_MERN-stack/',
+//   build: {
+//     rollupOptions: {
+//       external: ['axios']
+//     },
+//     outDir: 'dist',
+//   }
+// };
+
+
+
+
+
+
+export default {
   base: '/Calculator_MERN-stack/',
-  plugins: [react()],
-  server:{
-    proxy: {
-      '/api': 'http://localhost:3000',
-    },
-  },
   build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
+    outDir: 'dist',
   }
-})
+};
